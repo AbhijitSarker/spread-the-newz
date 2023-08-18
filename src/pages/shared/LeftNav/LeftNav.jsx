4a95ff97adb36d6a30606bbf3aa5691d6576ee10
AbhijitSarker <abhijitsarker03@gmail.com>
2023-08-18 18:10:56 +0600

    loading categories

--- src/pages/shared/LeftNav/LeftNav.jsx
+++ src/pages/shared/LeftNav/LeftNav.jsx
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const LeftNav = () => {
+    const [categories, setCategories] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:3000/categories')
+            .then(res => res.json())
+            .then(data => setCategories(data))
+            .catch(error => console.error(error))
+    }, [])
     return (
         <div>
-            leftt
+            <h4>All Category</h4>
+            <div>
+                {
+                    categories.map(category => <p
+                        key={category.id}
+                    >
+                        <Link to={`/category/${category.id}`} className='.hovertext-black text-decoration-none text-secondary'>{category.name}</Link>
+                    </p>)
+                }
+            </div>
         </div>
     );
 };
--- src/pages/shared/RightNav/RightNav.jsx
+++ src/pages/shared/RightNav/RightNav.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
-
+import { Button, ListGroup } from 'react-bootstrap';
+import { FaGithub, FaGoogle, FaInstagram, FaFacebook, FaTwitter } from 'react-icons/fa';
+import QZone from '../QZone/QZone';
+import bg from '../../../assets/bg.png'
 const RightNav = () => {
     return (
         <div>
-            right
+            <h4>Log In With</h4>
+            <Button className='w-100' variant="outline-danger"><FaGoogle />  Login With Google</Button>
+            <Button className='w-100 mt-2' variant="outline-success"><FaGithub />  Login With GitHub</Button>
+            <div className='mt-5'>
+                <h4>Find Us on </h4>
+                <ListGroup className='text-center'>
+                    <ListGroup.Item><FaFacebook />   Facebok</ListGroup.Item>
+                    <ListGroup.Item><FaInstagram />   Instagram </ListGroup.Item>
+                    <ListGroup.Item><FaTwitter />   Twitter</ListGroup.Item>
+
+                </ListGroup>
+            </div>
+            <QZone></QZone>
+            <div>
+                <img src={bg} alt="" />
+            </div>
         </div>
     );
 };
