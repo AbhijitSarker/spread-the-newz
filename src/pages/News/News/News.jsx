b283a2d0cdcee91b46edd91226bcd88ea40bcee4
AbhijitSarker <abhijitsarker03@gmail.com>
2023-08-19 12:52:30 +0600

    news details and indights

--- src/pages/News/News/News.jsx
+++ src/pages/News/News/News.jsx
@@ -1,10 +1,39 @@
 import React from 'react';
+import { Button, Card } from 'react-bootstrap';
+import { FaArrowLeft } from 'react-icons/fa';
+import { Link, useLoaderData } from 'react-router-dom';
+import Insights from '../Insights/Insights';
 
 const News = () => {
+    const news = useLoaderData()
+    const {
+        title,
+        details,
+        _id,
+        image_url,
+        category_id,
+        author,
+        author_image,
+        total_view,
+        rating
+    } = news;
     return (
         <div>
-            news
+            <Card >
+                <Card.Img variant="top" src={image_url} />
+                <Card.Body>
+                    <Card.Title>{title} </Card.Title>
+                    <Card.Text>
+                        {details}
+                    </Card.Text>
+                    <Link to={`/category/${category_id}`}>
+                        <Button variant="danger"> <FaArrowLeft></FaArrowLeft> Go Back</Button>
+                    </Link>
+                </Card.Body>
+            </Card>
+            <Insights></Insights>
         </div>
+
     );
 };
 
